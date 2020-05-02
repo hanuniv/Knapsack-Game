1f2b2b659c6c49122435da7b1f6cbd1324addd7e
Han Feng <han_feng@berkeley.edu>
2020-05-01 20:07:27 -0700

    loop over selected files

--- src/game_utils.py
+++ src/game_utils.py
@@ -9,7 +9,7 @@ import gambit
 TESTDIR = "tests/"
 
 
-def ls(suffix='.json'):
+def ls(prefix='', suffix='.json', substr=''):
     """
     list file names with given suffix without arguments, if arguments are given, loop over given files (without suffix)
     """
@@ -17,7 +17,7 @@ def ls(suffix='.json'):
         jslist = [TESTDIR + f + suffix for f in sys.argv[1:]]
     else:
         jslist = [os.path.join(root, f) for root, _, files in os.walk(TESTDIR)
-                  for f in files if f.endswith(suffix)]
+                  for f in files if f.endswith(suffix) and f.startswith(prefix) and substr in f]
         print("No knapsack json file listed. The test foler contains: \n \t ", jslist)
         y = input("Go over all of them? [y/n] > ")
         if y.lower() != 'y':
@@ -29,6 +29,12 @@ def ls(suffix='.json'):
 def dumpjs(js, filename):
     with open(TESTDIR + filename+'.json', 'w') as lf:
         json.dump(js, lf, indent=4)
+
+def stat(filename):
+    with open(TESTDIR + filename+'_stat.json', 'r') as fjs:
+        data=json.load(fjs)
+    data = np.array(data)
+    return np.mean(data, axis=0), np.std(data, axis=0)
 
 class KnapsackGame:
     def __init__(self, js, gen_strategies=False):
